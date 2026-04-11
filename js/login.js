99484725df981f4b3028bb88aa76fae3a9f71097
Luis26-star <fraglu@yahoo.com.br>
2026-04-11 19:59:34 +0200

    Create login.js

---
+++ js/login.js
@@ -0,0 +1,17 @@
+function login(){
+
+const password = document.getElementById("password").value;
+
+if(password === "acordes2026"){
+
+window.location.href = "chorbereich.html";
+
+}
+
+else{
+
+alert("Falsches Passwort");
+
+}
+
+}
