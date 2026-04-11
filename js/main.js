f8dc7e1bcb849104527c717a0574df2b84481c43
Luis26-star <fraglu@yahoo.com.br>
2026-04-11 13:41:19 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,3 +6,24 @@ menuToggle.addEventListener("click", () => {
 menu.classList.toggle("active");
 
 });
+
+
+
+/* Countdown */
+
+const concertDate = new Date("June 15, 2026 19:30:00").getTime();
+
+const timer = setInterval(function(){
+
+const now = new Date().getTime();
+const distance = concertDate - now;
+
+const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+const hours = Math.floor((distance % (1000*60*60*24)) / (1000*60*60));
+const minutes = Math.floor((distance % (1000*60*60)) / (1000*60));
+
+document.getElementById("days").innerHTML = days;
+document.getElementById("hours").innerHTML = hours;
+document.getElementById("minutes").innerHTML = minutes;
+
+},1000);
