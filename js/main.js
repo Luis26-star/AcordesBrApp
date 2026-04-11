d024ab14f8535fa6606a5d6d49e83f23ea371633
Luis26-star <fraglu@yahoo.com.br>
2026-04-11 12:57:26 +0200

    Add mobile menu toggle functionality

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+const menu = document.querySelector(".menu");
+const menuToggle = document.querySelector("#mobile-menu");
 
+menuToggle.addEventListener("click", () => {
+
+menu.classList.toggle("active");
+
+});
