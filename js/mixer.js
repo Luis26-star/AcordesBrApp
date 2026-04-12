db1ac73303df6e654673473423815b69ad74ce2f
Luis26-star <fraglu@yahoo.com.br>
2026-04-12 16:15:38 +0200

    Update mixer.js

--- js/mixer.js
+++ js/mixer.js
@@ -1,1 +1,29 @@
+function setTempo(speed){
+document.getElementById("sopran").playbackRate = speed;
+document.getElementById("alt").playbackRate = speed;
+document.getElementById("tenor").playbackRate = speed;
+document.getElementById("bass").playbackRate = speed;
+}
 
+let loopInterval;
+
+function startLoop(){
+
+let start = document.getElementById("loopStart").value;
+let end = document.getElementById("loopEnd").value;
+
+loopInterval = setInterval(function(){
+
+let audio = document.getElementById("sopran");
+
+if(audio.currentTime >= end){
+audio.currentTime = start;
+}
+
+},200);
+
+}
+
+function stopLoop(){
+clearInterval(loopInterval);
+}
