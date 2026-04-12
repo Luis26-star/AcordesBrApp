33e98c2cf737b840b723870c0d4da5e30491f829
Luis26-star <fraglu@yahoo.com.br>
2026-04-12 14:40:29 +0200

    Update player.js

--- js/player.js
+++ js/player.js
@@ -1,1 +1,33 @@
+const songs = ["canto_ossanha"];
 
+const voices = ["sopran","alt","tenor","bass"];
+
+const container = document.getElementById("song-list");
+
+songs.forEach(song => {
+
+  let block = document.createElement("div");
+
+  block.innerHTML = "<h3>" + song + "</h3>";
+
+  voices.forEach(v => {
+
+    let btn = document.createElement("button");
+
+    btn.innerText = v + " ▶";
+
+    btn.onclick = () => {
+
+      let audio = new Audio("audio/" + song + "/" + v + ".mp3");
+
+      audio.play();
+
+    };
+
+    block.appendChild(btn);
+
+  });
+
+  container.appendChild(block);
+
+});
