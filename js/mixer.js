96aaabe7510ba64c20e3ade3190eed821818cfdb
Luis26-star <fraglu@yahoo.com.br>
2026-04-12 16:49:46 +0200

    Update mixer.js

--- js/mixer.js
+++ js/mixer.js
@@ -43,3 +43,58 @@ document.getElementById("tenor").pause();
 document.getElementById("bass").pause();
 
 }
+const songs = [
+
+{
+title: "Canto de Ossanha",
+folder: "canto_ossanha"
+},
+
+{
+title: "Choro Bandido",
+folder: "choro_bandido"
+}
+
+];
+
+const voices = ["sopran","alt","tenor","bass"];
+
+const container = document.getElementById("songList");
+
+songs.forEach(song => {
+
+let block = document.createElement("div");
+
+block.innerHTML = `<h3>🎵 ${song.title}</h3>`;
+
+voices.forEach(v => {
+
+let audio = document.createElement("audio");
+
+audio.id = song.folder + "_" + v;
+
+audio.src = "audio/" + song.folder + "/" + v + ".mp3";
+
+container.appendChild(audio);
+
+});
+
+let playAll = document.createElement("button");
+
+playAll.innerText = "▶ Alle Stimmen";
+
+playAll.onclick = () => {
+
+voices.forEach(v => {
+
+document.getElementById(song.folder + "_" + v).play();
+
+});
+
+};
+
+block.appendChild(playAll);
+
+container.appendChild(block);
+
+});
