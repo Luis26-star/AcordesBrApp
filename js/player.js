c555b64fb93ae7e7133f02a0a66e9905c714884b
Luis26-star <fraglu@yahoo.com.br>
2026-04-12 14:58:08 +0200

    Update player.js

--- js/player.js
+++ js/player.js
@@ -8,18 +8,30 @@ songs.forEach(song => {
 
   let block = document.createElement("div");
 
-  block.innerHTML = "<h3>" + song + "</h3>";
+  block.style.marginBottom = "30px";
+
+  let title = document.createElement("h3");
+  title.innerText = song.replace("_"," ");
+
+  block.appendChild(title);
 
   voices.forEach(v => {
 
     let btn = document.createElement("button");
 
-    btn.innerText = v + " ▶";
+    btn.innerText = "▶ " + v;
+
+    btn.style.margin = "5px";
+    btn.style.padding = "8px 14px";
+    btn.style.borderRadius = "20px";
+    btn.style.border = "none";
+    btn.style.background = "#2e7d32";
+    btn.style.color = "white";
+    btn.style.cursor = "pointer";
 
     btn.onclick = () => {
 
-      let audio = new Audio("audio/" + song + "/" + v + ".mp3");
-
+      let audio = new Audio(`audio/${song}/${v}.mp3`);
       audio.play();
 
     };
